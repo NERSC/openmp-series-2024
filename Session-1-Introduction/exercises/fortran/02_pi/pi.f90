3e5dba7e9c8b5c421d2d04deba4fe4b3bd226e6a
helenhe40 <yhe@lbl.gov>
2024-05-03 14:13:02 -0700

    add Session 1 slides and exercises

---
+++ Session-1-Introduction/exercises/fortran/02_pi/pi.f90
@@ -0,0 +1,71 @@
+!
+!  OpenMP lecture exercises
+!  Copyright (C) 2011 by Christian Terboven <terboven@rz.rwth-aachen.de>
+! 
+!  This program is free software; you can redistribute it and/or modify
+!  it under the terms of the GNU General Public License as published by
+!  the Free Software Foundation; either version 2 of the License, or
+!  (at your option) any later version.
+! 
+!  This program is distributed in the hope that it will be useful,
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!  GNU General Public License for more details.
+! 
+!  You should have received a copy of the GNU General Public License
+!  along with this program; if not, write to the Free Software
+!  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+! 
+!
+
+function f(a)
+double precision :: f
+double precision, intent(in) :: a 
+f = 4.0/(1.0 + a*a)
+end function f 
+
+
+function CalcPi(n)
+double precision :: CalcPi
+integer, intent(in) :: n
+double precision :: fH, fSum, fX, f
+integer :: i
+fH = 1.0 / n
+fSum = 0.0
+
+do i=0,n
+	fX = fH * ( 0.5 + i )
+	fSum = fSum + f(fX)	
+end do
+ CalcPi = fH * fSum
+end function CalcPi
+
+
+
+program main
+use omp_lib
+implicit none
+integer :: n = 150000000
+double precision :: fPi25DT = 3.141592653589793238462643
+double precision :: fPi, calcpi
+double precision :: fTimeStart, fTimeEnd
+
+
+if ( n <= 0 .or. n > 2147483647) then
+	write(*,*),'given value has to be between 0 and 2147483647'
+	stop
+end if
+
+fTimeStart = omp_get_wtime()
+
+! The calculation is done here
+fPi = calcpi(n)
+
+fTimeEnd = omp_get_wtime()
+
+ write (*,100) fPi, abs(fPi - fPi25DT)
+ 100 format ('pi is approximately = ', e15.8 ,' , Error =' , e15.8)
+ write (*,*)'wall clock time	=', fTimeEnd-fTimeStart
+ 
+ end program main
+
---
+++ Session-1-Introduction/exercises/fortran/05_minmaxreduction/solution/MinMaxReduction.f90
@@ -0,0 +1,57 @@
+function func(i)
+double precision :: func
+integer, intent(in) :: i
+	func = sin(i * cos(i*1.))
+end function func
+
+program main
+use omp_lib
+implicit none
+integer :: dimensio = 9999999 
+double precision :: dMin, dMax
+double precision, dimension(:), allocatable :: dArray
+integer :: i
+double precision :: func,t1,t2
+
+allocate (dArray(dimensio))
+
+t1 = omp_get_wtime()
+
+!$omp parallel
+!$omp do reduction(min: dMin) reduction(max: dMax)
+	do i=1, dimensio
+		dArray(i) = func(i)
+		dMin = min(dMin, dArray(i))
+		dMax = max(dMax, dArray(i))
+	end do
+!$omp end parallel
+
+ deallocate (dArray)
+
+t2 = omp_get_wtime()
+
+write (*,*) 'Computation took ', t2-t1, 'seconds.'
+write (*,*) 'dMin is ', dMin
+write (*,*) 'dMax is ', dMax
+
+end program main
+
+!
+!  OpenMP lecture exercises
+!  Copyright (C) 2011 by Christian Terboven <terboven@rz.rwth-aachen.de>
+!
+!  This program is free software; you can redistribute it and/or modify
+!  it under the terms of the GNU General Public License as published by
+!  the Free Software Foundation; either version 2 of the License, or
+!  (at your option) any later version.
+!
+!  This program is distributed in the hope that it will be useful,
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!  GNU General Public License for more details.
+!
+!  You should have received a copy of the GNU General Public License
+!  along with this program; if not, write to the Free Software
+!  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+!
+!
